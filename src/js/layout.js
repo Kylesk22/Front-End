fb6272a07fdda9ffb432bcea24169aa2b3304e51
Kylesk22 <Kylesk22@gmail.com>
2022-10-19 23:44:55 +0000

    initial

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import { Gym } from "./views/gym";
+import { User } from "./views/user";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -25,11 +25,11 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
-							<Demo />
+						<Route exact path="/myGym">
+							<Gym />
 						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
+						<Route exact path="/account/:user_id">
+							<User />
 						</Route>
 						<Route>
 							<h1>Not found!</h1>
