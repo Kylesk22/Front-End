1f25b4e4db99e0433f234f613b585b4392f8b106
Kylesk22 <Kylesk22@gmail.com>
2022-10-27 21:29:44 +0000

    login

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,21 +1,97 @@
+import { Navigate } from "react-router";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			token: null,
+			email: "",
+			loggedIn: false,
+			
+			
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
+			syncTokenFromSessionStore: () => {
+				const token = sessionStorage.getItem("token");
+				if (token && token != "" && token != undefined) setStore({token: token})
+			},
+
+			logout: () => {
+				sessionStorage.removeItem("token");
+				setStore({token: null})
+				setStore({email: null})
+			},
+			
+			signup: async (email, password, first_name, last_name) => {
+				const ops = {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password,
+						"first-name": first_name,
+						"last_name": last_name
+					})
+
+				}
+
+				try {
+				const resp = await fetch('https://3001-kylesk22-backend-v1vfho00s6i.ws-us71.gitpod.io/api/signup', {ops})
+				if (resp.status !== 200) {
+				alert("There has been an error")
+				return false
+				}
+				const data = await resp.json();		
+				sessionStorage.setItem("token", data.access_token)
+				setStore({token: data.access_token})
+				actions.setStore({email: email})
+					
+				}
+				catch (err) {
+					console.error(err);
+				}
+			},
+
+			login: async(email, password) => {
+				const opts = {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password
+					})
+					}
+
+					try{
+					const resp = await fetch('https://3001-kylesk22-backend-v1vfho00s6i.ws-us72.gitpod.io/api/login', opts)
+					if (resp.status !== 200) {
+						alert("There has been an error")
+						return false
+					}
+					
+					const data = await resp.json();		
+					sessionStorage.setItem("token", data.access_token)
+					setStore({token: data.access_token})
+					actions.setStore({email: email})
+					actions.setStore({loggedIn: true})
+					console.log(store.loggedIn)
+					return true
+					}
+					catch(error) {
+						console.error("There has been an error")
+					}
+				},
+
+
+
+			setEmail: (e) => {
+				setStore({email: e})
+			},
+
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,60 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
 import "../../styles/home.css";
+import { Navigate } from "react-router";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleClick = () => {
+		actions.login(email, password)
+	};
+	
+
+	return(
+		<div>
+
+			<div className="row" style={{"textAlign": "center"}}>
+				<div className="col-6">
+					<h2 className="display-4">Find your</h2>
+					<h1 className="display-4">GYM</h1>
+					<h2 className="display-4">Find your</h2>
+					<h1 className="display-4">HOME</h1>
+
+					
+				</div>
+				{(!store.loggedIn)? 
+				<div className="col-6">
+					<div className="form" onSubmit={() => alert("submitted")}>
+						<h3>Login</h3>
+						<input type= "text" placeholder="Email" value= {email} onChange={(e) => setEmail(e.target.value)}></input>
+						<br/>
+						<input type= "password" placeholder ="Password" style={{"margin": "8px"}} value={password} onChange={(e)=> setPassword(e.target.value)}></input>
+						<br/>
+						<Link to="/forgot_password">
+							<span>Forgot Password?</span>
+						</Link>
+						<br/>
+						
+						<span className="btn btn-primary btn-md mt-1" href="#" role="button" onClick={handleClick}>Login</span>
+						
+						<hr className="mt-4 mx-5" />
+						<span>New User?</span>
+						<br/>
+						<Link to="/register">
+							<button type="button" className="btn btn-primary mt-2">Register</button>
+						</Link>
+
+				
+					</div>
+				</div> : Navigate('/account/:user_id')}
+				
+			</div>
+		</div>
+	)
+}
