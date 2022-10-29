ae69bca17c34401850b88cccbfe95b2fc65d4def
SantaGosu <taz.c.munoz@gmail.com>
2022-10-29 04:01:07 +0000

    put api key back in .env file

--- src/js/views/gym.js
+++ src/js/views/gym.js
@@ -14,27 +14,26 @@ import {
 } from "@reach/combobox";
 
 export const Gym = () => {
+	
 	const {isLoaded} = useLoadScript({
 		googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-		libraries: ['places'],
 	})
 
 	if (!isLoaded) return <div>Loading...</div>
-	return <Map />
+	return (<Map />)
 };
 
 const Map = () => {
 	const center = useMemo(() => ({ lat:25.761681, lng: -80.191788 }), []);
 	const [selected, setSelected] = useState(null);
     return (
-		<>
+		<>	
             <div className="placeContainer">
 				<PlacesAutocomplete setSelected={setSelected} />
 			</div>
-
         <GoogleMap zoom={15} 
-		center={center}
-		mapContainerClassName="mapContainer">
+			center={center}
+			mapContainerClassName="mapContainer">
 			{selected && <Marker position={selected} />}
 		</GoogleMap>
 		</>
@@ -55,7 +54,7 @@ const PlacesAutocomplete = ({ setSelected }) => {
 			clearSuggestions();
 
 			const result = await getGeocode({address});
-			const {lat, lng} = await getLatLng(result[0]);
+			const {lat, lng} = getLatLng(result[0]);
 			setSelected({lat, lng});
 		}
 
@@ -65,7 +64,10 @@ const PlacesAutocomplete = ({ setSelected }) => {
                 value={value} onChange={(e) => setValue(e.target.value)} disabled={!ready} className="combobox-input"/>
 			<ComboboxPopover>
 				<ComboboxList>
-					{status === "OK" && data.map(({place_id, description}) => <ComboboxOption key={place_id} value={description} />)}
+					{status === 200 && 
+						data.map(({place_id, description}) => {
+					<ComboboxOption key={place_id} value={description} />
+						})}
                 </ComboboxList>
 			</ComboboxPopover>
 		</Combobox>
